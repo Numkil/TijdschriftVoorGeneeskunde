1c1311ba4a8ce9754426b71f1e05c66c39373c9f
Numkil <kevin.jossart@hotmail.com>
2015-05-24 10:03:16 +0200

    Finish up the user object according to uml

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace AppBundle\Entity;
+
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * Class User
@@ -14,17 +16,187 @@ class User extends BaseUser
 {
     /**
      * @ORM\Id
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="integer", name="id")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    protected $id;
+    protected $_id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=150)
+     */
+    protected $_name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstname", type="string", length=150)
+     */
+    protected $_firstname;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="telephone", type="integer", length=150)
+     */
+    protected $_telephone;
+
+    /**
+     * @var Role[]
+     *
+     * @ORM\ManyToMany(targetEntity="Role", inversedBy="users")
+     */
+    protected $_roles;
+
+    /**
+     * @var Subscriber
+     *
+     * ORM\OneToOne(targetEntity="Subscriber", inversedBy="user")
+     * @ORM\JoinColumn(name="subscriber", nullable=true, referencedColumnName="id")
+     */
+    protected $_subscriber;
+
+    /**
+     * @var Bookstore
+     *
+     * ORM\ManyToOne(targetEntity="Bookstore", inversedBy="users")
+     * @ORM\JoinColumn(name="bookstore", nullable=true, referencedColumnName="id")
+     */
+    protected $_bookstore;
+
+    /**
+     *
+     */
     public function __construct()
     {
         parent::__construct();
         #hack hack hack
     }
 
-    #hack hack hack
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->_name;
+    }
 
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return this
+     */
+    public function setName($name)
+    {
+        $this->_name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->_firstname;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return this
+     */
+    public function setFirstName($name)
+    {
+        $this->_firstname = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get Telephone
+     *
+     * @return string
+     */
+    public function getTelephone()
+    {
+        return $this->_telephone;
+    }
+
+    /**
+     * Set name
+     *
+     * @param integer $telephone
+     * @return this
+     */
+    public function setTelephone($telephone)
+    {
+        $this->_telephone = $telephone;
+
+        return $this;
+    }
+
+    /**
+     * @return Role[] The user roles
+     */
+    public function getRoles()
+    {
+        return $this->_roles->toArray();
+    }
+
+    /**
+     * @param Role[] role
+     * @return this
+     */
+    public function setRoles(array $roles)
+    {
+        $this->_roles = new ArrayCollection($roles);
+
+        return $this;
+    }
+
+    /**
+     * @return Subscriber
+     */
+    public function getSubscriber()
+    {
+        return $this->_subscriber;
+    }
+
+    /**
+     * @param Subscriber
+     * @return this
+     */
+    public function setSubscriber($subscriber)
+    {
+        $this->_subscriber = $subscriber;
+
+        return $this;
+    }
+
+    /**
+     * @return Bookstore
+     */
+    public function getBookstore()
+    {
+        return $this->_bookstore;
+    }
+
+    /**
+     * @param Bookstore
+     * @return this
+     */
+    public function setBookstore($bookstore)
+    {
+        $this->_bookstore = $bookstore;
+
+        return $this;
+    }
 }
