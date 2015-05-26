2828179036c737a44841f37d207ca5273c90fe38
titmastercrunch <bcurias@gmail.com>
2015-05-26 13:08:36 +0200

    created notice index page

--- src/AppBundle/Controller/NoticeController.php
+++ src/AppBundle/Controller/NoticeController.php
@@ -4,31 +4,50 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Form\Type\NoticeFormType;
 use AppBundle\Entity\Notice;
 class NoticeController extends Controller{
 	
 	/**
-	* @Route("/notice/overview", name="noticeOverview")
+	* @Route("/notice", name="noticeOverview")
 	*/
 	public function indexAction(){
-		return $this->render('notice/index.html.twig');
+		$notices = $this->getDoctrine()
+			->getRepository('AppBundle:Notice')
+			->findBy(array(), array('creationDate' => 'DESC'));
+
+		$parameters = array(
+			"notices" => $notices
+			);
+
+		return $this->render('notice/index.html.twig', $parameters);
 	}
 
 	/**
 	* @Route("/notice/new", name="createNotice")
 	*/
-	public function createNoticeAction(){
+	public function createNoticeAction(Request $request){
 		$noticeObject = new Notice();
 
-		$notice = $this->createForm(new NoticeFormType(), $noticeObject);
+		$form = $this->createForm(new NoticeFormType(), $noticeObject);
+
+		$form->handleRequest($request);
+
+		if ($form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($noticeObject);
+			$em->flush();
+        
+			return $this->redirectToRoute('noticeOverview');
+	    }
 
 		$parameters = array(
-							"notice" => $notice->createView(),
+							"notice" => $form->createView(),
 						);
 
 		return $this->render('notice/newNotice.html.twig', $parameters);
 	}
+
 
 }
