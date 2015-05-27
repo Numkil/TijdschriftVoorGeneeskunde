28088c8eef19960af951c0057b252d1c58f273fd
Numkil <kevin.jossart@hotmail.com>
2015-05-27 10:46:34 +0200

    Layout and working edit account and password change

---
+++ src/AppBundle/Form/Type/ProfileEditFormType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use libphonenumber\PhoneNumberFormat;
+
+class ProfileEditFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        // add your custom field
+        $builder->add('name');
+        $builder->add('firstname');
+        $builder->add('telephone', 'tel', array('default_region' => 'BE',
+            'format' => PhoneNumberFormat::NATIONAL));
+    }
+
+    public function getParent()
+    {
+        return 'fos_user_profile';
+    }
+
+    public function getName()
+    {
+        return 'app_user_profile';
+    }
+}
